4952df6c5849712d3459485ccf99c40c217db827
Heathcliff <dennis.nathan@gmail.com>
2016-11-19 18:09:42 -0700

    Working routes for race and color change on 2 spheros, run gulp.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,20 +2,26 @@ const express = require('express');
 const router = express.Router();
 var sphero = require('sphero');
 var orb = sphero('/dev/tty.Sphero-PRO-RN-SPP');
-// var orb2 = sphero('/dev/tty.Sphero-ORR-RN-SPP');
+var orb2 = sphero('/dev/tty.Sphero-BBO-RN-SPP');
 
 orb.connect(function() {
   orb.color('green');
-  // orb2.color('red');
+});
+
+orb2.connect(function() {
+  orb2.color('red');
 });
 
 router.get('/', function (req, res, next) {
   res.status(200).send('success');
-})
+});
 
-router.get('/:color', function (req, res, next) {
+router.get('/:color/:color2', function (req, res, next) {
   var newColor = req.params.color;
+  var newColor2 = req.params.color2;
   orb.color(newColor);
+  orb2.color(newColor2);
+  res.status(200).send('success');
 });
 
 router.get('/:speed/:direction/:time', function(req, res, next) {
@@ -27,18 +33,17 @@ router.get('/:speed/:direction/:time', function(req, res, next) {
     orb.roll(speed, direction);
   }, time);
 
-  setInterval(function() {
-    orb2.roll((speed-50), direction);
-    res.status(200).send('success');
-  }, time);
+  // setInterval(function() {
+  //   orb2.roll((speed - 50), direction);
+  // }, time);
 });
 
-orb.setPowerNotification(1, function (err, data) {
-  console.log(err | 'data: ' + data);
-});
-
-orb2.setPowerNotification(1, function (err, data) {
-  console.log(err | 'data: ' + data);
-});
+// orb.setPowerNotification(1, function (err, data) {
+//   console.log(err | 'data: ' + data);
+// });
+//
+// orb2.setPowerNotification(1, function (err, data) {
+//   console.log(err | 'data: ' + data);
+// });
 
 module.exports = router;
